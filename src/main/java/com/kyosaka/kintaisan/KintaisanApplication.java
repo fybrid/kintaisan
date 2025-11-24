10b44d97e02424000f9e0b5c125169cb52a1be49
Fybrid <sfybrid@gmail.com>
2025-11-24 15:01:19 +0900

    :tada: first commit

---
+++ src/main/java/com/kyosaka/kintaisan/KintaisanApplication.java
@@ -0,0 +1,20 @@
+package com.kyosaka.kintaisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RestController
+public class KintaisanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaisanApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/kyosaka/kintaisan/KintaisanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyosaka.kintaisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaisanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
