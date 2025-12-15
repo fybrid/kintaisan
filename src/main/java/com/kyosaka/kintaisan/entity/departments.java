bd217e09f4cd68a8754176fbeab62aa4524115c9
zebUran <sugarzebura@gmail.com>
2025-12-15 13:31:49 +0900

    12/15 entityの更新2

---
+++ src/main/java/com/kyosaka/kintaisan/entity/departments.java
@@ -0,0 +1,46 @@
+package com.kyosaka.kintaisan.entity;
+
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "departments")
+@Data
+public class departments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "department_id", unique = true)
+    private Long departmentId;
+
+    @Column(name = "department_name", unique = true, length = 20)
+    private Long departmentName;
+
+    @Column(name = "created_at")
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private ZonedDateTime updatedAt;
+
+    @PrePersist//作成時にcreatedAt, updatedAtを登録する
+    protected void onCreate(){
+        createdAt = ZonedDateTime.now();
+        updatedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate//更新時にupdatedAtを書き換える
+    protected void onUpdate(){
+        updatedAt = ZonedDateTime.now();
+    }
+    
+}
---
+++ src/main/java/com/kyosaka/kintaisan/entity/workplaces.java
@@ -0,0 +1,46 @@
+package com.kyosaka.kintaisan.entity;
+
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "workplaces")
+@Data
+public class workplaces {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "workplace_id", unique = true)
+    private Long workplaceId;
+
+    @Column(name = "workplace_name", unique = true, length = 20)
+    private Long workplaceName;
+
+    @Column(name = "created_at")
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private ZonedDateTime updatedAt;
+
+    @PrePersist//作成時にcreatedAt, updatedAtを登録する
+    protected void onCreate(){
+        createdAt = ZonedDateTime.now();
+        updatedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate//更新時にupdatedAtを書き換える
+    protected void onUpdate(){
+        updatedAt = ZonedDateTime.now();
+    }
+    
+}
