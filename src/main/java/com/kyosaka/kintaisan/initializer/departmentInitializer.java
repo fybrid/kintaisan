e42628d8e6c9e1db6c02c8eb7430c136dbd283a4
Fybrid <sfybrid@gmail.com>
2026-01-06 16:11:12 +0900

    :wrench: seedデータの数を追加

--- src/main/java/com/kyosaka/kintaisan/initializer/departmentInitializer.java
+++ src/main/java/com/kyosaka/kintaisan/initializer/departmentInitializer.java
@@ -3,6 +3,9 @@ package com.kyosaka.kintaisan.initializer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.kyosaka.kintaisan.entity.departments;
 import com.kyosaka.kintaisan.repository.departmentsRepository;
 
@@ -16,15 +19,38 @@ public class departmentInitializer implements CommandLineRunner{
 
   @Override
   public void run (String... args) {
-    if (departmentsRepository.existsByDepartmentId(1)){
+    if (departmentsRepository.existsByDepartmentId(1)
+        && departmentsRepository.existsByDepartmentId(2)
+        && departmentsRepository.existsByDepartmentId(3)) {
       return;
     }
 
-    departments department = new departments();
-    department.setDepartmentId(1);
-    department.setDepartmentName("開発部");
+    List<departments> departmentsList = new ArrayList<>();
 
-    departmentsRepository.save(department);
+    if (!departmentsRepository.existsByDepartmentId(1)) {
+      departments department = new departments();
+      department.setDepartmentId(1);
+      department.setDepartmentName("開発部");
+      departmentsList.add(department);
+    }
+
+    if (!departmentsRepository.existsByDepartmentId(2)) {
+      departments department = new departments();
+      department.setDepartmentId(2);
+      department.setDepartmentName("営業部");
+      departmentsList.add(department);
+    }
+
+    if (!departmentsRepository.existsByDepartmentId(3)) {
+      departments department = new departments();
+      department.setDepartmentId(3);
+      department.setDepartmentName("人事部");
+      departmentsList.add(department);
+    }
+
+    if (!departmentsList.isEmpty()) {
+      departmentsRepository.saveAll(departmentsList);
+    }
   }
 
 }
--- src/main/java/com/kyosaka/kintaisan/initializer/workplaceInitializer.java
+++ src/main/java/com/kyosaka/kintaisan/initializer/workplaceInitializer.java
@@ -3,6 +3,9 @@ package com.kyosaka.kintaisan.initializer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.kyosaka.kintaisan.entity.workplaces;
 import com.kyosaka.kintaisan.repository.workplacesRepository;
 
@@ -16,14 +19,37 @@ public class workplaceInitializer implements CommandLineRunner {
 
   @Override
   public void run (String... args) {
-    if (workplacesRepository.existsByWorkplaceId(1)){
+    if (workplacesRepository.existsByWorkplaceId(1)
+        && workplacesRepository.existsByWorkplaceId(2)
+        && workplacesRepository.existsByWorkplaceId(3)) {
       return;
     }
 
-    workplaces workplace = new workplaces();
-    workplace.setWorkplaceId(1);
-    workplace.setWorkplaceName("本社");
+    List<workplaces> workplacesList = new ArrayList<>();
 
-    workplacesRepository.save(workplace);
+    if (!workplacesRepository.existsByWorkplaceId(1)) {
+      workplaces workplace = new workplaces();
+      workplace.setWorkplaceId(1);
+      workplace.setWorkplaceName("本社");
+      workplacesList.add(workplace);
+    }
+
+    if (!workplacesRepository.existsByWorkplaceId(2)) {
+      workplaces workplace = new workplaces();
+      workplace.setWorkplaceId(2);
+      workplace.setWorkplaceName("自宅");
+      workplacesList.add(workplace);
+    }
+
+    if (!workplacesRepository.existsByWorkplaceId(3)) {
+      workplaces workplace = new workplaces();
+      workplace.setWorkplaceId(3);
+      workplace.setWorkplaceName("支社");
+      workplacesList.add(workplace);
+    }
+
+    if (!workplacesList.isEmpty()) {
+      workplacesRepository.saveAll(workplacesList);
+    }
   }
 }
