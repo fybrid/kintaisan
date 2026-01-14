0309898f3f6d8d8ecb9ba7e5dd3aa9918962c38b
Fybrid <sfybrid@gmail.com>
2026-01-14 23:07:20 +0900

    :sparkles: アカウント一覧ページのたたきを作成

---
+++ src/main/java/com/kyosaka/kintaisan/controller/AdminAccountController.java
@@ -0,0 +1,17 @@
+package com.kyosaka.kintaisan.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kyosaka.kintaisan.service.UserAccountService;
+
+@RequestMapping("/admin")
+public class AdminAccountController {
+
+  // private final UserAccountService userAccountService;
+
+  // @PostMapping("/users/list")
+  // public String getUser() {
+
+  // }
+}
