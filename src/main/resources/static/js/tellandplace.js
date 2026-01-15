5596ecf73378fcf7e82a931e986dbe2104718880
stelvio <stelvio902010@outlook.jp>
2026-01-15 16:30:23 +0900

    ✨cssの追加 ✨モーダル表示の追加

--- src/main/resources/static/js/tellandplace.js
+++ src/main/resources/static/js/tellandplace.js
@@ -1,10 +1,23 @@
-// 戻るボタン（前のページへ戻る）
 document.addEventListener("DOMContentLoaded", () => {
   const backBtn = document.getElementById("back-btn");
 
   if (backBtn) {
-    backBtn.addEventListener("click", () => {
+    backBtn.addEventListener("click", (e) => {
+      e.preventDefault();
       history.back();
     });
   }
+
+  window.showModal = function() {
+    const modalOverlay = document.getElementById("modalOverlay");
+    const modal = document.getElementById("modal");
+
+    if (modalOverlay && modal) {
+      modalOverlay.style.display = "block";
+      modal.style.display = "block";
+    }
+  };
+
 });
+
+
