1168cf6aa54260c0b97226f11518ce340909b877
Fybrid <sfybrid@gmail.com>
2026-01-18 01:09:46 +0900

    :sparkles: アカウント編集ページをアカウント作成ページからコピー、ページのパスを配置

--- src/main/java/com/kyosaka/kintaisan/controller/AdminEditController.java
+++ src/main/java/com/kyosaka/kintaisan/controller/AdminEditController.java
@@ -1,5 +1,17 @@
 package com.kyosaka.kintaisan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("/admin")
 public class AdminEditController {
-  
+
+  @GetMapping("/edit")
+  public String showEditPage() {
+      return "accountEdit";
+  }
+
 }
